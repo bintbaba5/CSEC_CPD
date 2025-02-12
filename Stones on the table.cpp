44f21cd54330c3ca05cac422eecc9290fa813154
bintbaba5 <neimashafi29@gmail.com>
2025-02-12 17:02:00 +0300

    Create Stones on the table.cpp

---
+++ Stones on the table.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+
+    int removals = 0;
+    for (int i = 1; i < n; ++i) {
+        if (s[i] == s[i - 1]) {
+            removals++;
+        }
+    }
+
+    cout << removals << endl;
+
+    return 0;
+}
