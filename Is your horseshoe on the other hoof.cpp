fc0d18b3e621946ba747a194f5fe9ebf825d872c
bintbaba5 <neimashafi29@gmail.com>
2025-02-12 18:02:13 +0300

    Create Is your horseshoe on the other hoof.cpp

---
+++ Is your horseshoe on the other hoof.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+int main() {
+    int s1, s2, s3, s4;
+    cin >> s1 >> s2 >> s3 >> s4;
+
+    set<int> uniqueColors;
+    uniqueColors.insert(s1);
+    uniqueColors.insert(s2);
+    uniqueColors.insert(s3);
+    uniqueColors.insert(s4);
+
+    int neededColors = 4 - uniqueColors.size();
+
+    cout << neededColors << endl;
+
+    return 0;
+}
